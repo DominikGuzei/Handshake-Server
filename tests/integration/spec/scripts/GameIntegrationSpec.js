91f0dac00664f481863018c803addb5482d3d1aa
Dominik Guzei <dominik_guzei@gmx.at>
2011-01-24 15:08:29 +0100

    Improve code quality of GameIntegrationSpec

--- tests/integration/spec/scripts/GameIntegrationSpec.js
+++ tests/integration/spec/scripts/GameIntegrationSpec.js
@@ -1,33 +1,42 @@
 	
 describe("Game Integration", function() {	
 	
+	// domain and key for localhost in the debug modus of the server
+	var key = "aewgj34jt20gj32jg2adfwey3j3hl";
+	var domain = "ws://localhost:8008/";
+	
+	var newGameRoute = "/new";
+	
+	var handshakeServerId = "server";
+	var handshakeAddEvent = "added";
+	
 	describe("Connecting", function() {
-		
-		var key = "jf41j2k41adfae12j4o2j1p2";
-		
+
 		it("should be possible to connect", function() {
 			runs(function() {
 				this.onOpenSpy = jasmine.createSpy();
-				this.websocket = new WebSocket("ws://localhost:8008/" + key + "/new");
+				this.websocket = new WebSocket(domain + key + newGameRoute);
 				this.websocket.onopen = this.onOpenSpy;
 			});
 			waits(200);
 			runs(function() {
 				expect(this.onOpenSpy).toHaveBeenCalled();
+				this.websocket.close();
 			});
 		});
 		
 		it("should receive a message with the game id", function() {
 			var message;
 			runs(function(){
-				this.websocket = new WebSocket("ws://localhost:8008/" + key + "/new");
+				this.websocket = new WebSocket(domain + key + newGameRoute);
 				this.websocket.onmessage = function(event) {
 					message = event.data;
 				}
 			});
 		  waits(300);
 			runs(function() {
-				expect(message).toEqual("server added {\"id\":0}");
+				expect(message).toEqual(handshakeServerId + " " + handshakeAddEvent + " {\"id\":0}");
+				//this.websocket.close();
 			});
 		});
 	
